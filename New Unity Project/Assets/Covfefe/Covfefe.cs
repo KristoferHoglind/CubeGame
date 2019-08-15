85aa93ceea5f364396ba60ba346a35eebb5ec2a0
Kristofer Janukiewicz <kristofer.janukiewicz@cybercom.com>
2019-08-15 10:44:41 +0200

    Upload the CubeGame

---
+++ New Unity Project/Assets/Covfefe/Covfefe.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Covfefe : MonoBehaviour
+{
+    void Start()
+    {
+        Renderer rend = GetComponent<Renderer>();
+
+        rend.material.shader = Shader.Find("_Color");
+        rend.material.SetColor("_Color", new Color(62f/255f, 10f/255f, 10f/255f, 0.75f));
+
+        rend.material.shader = Shader.Find("Specular");
+        rend.material.SetColor("_SpecColor", new Color(1f, 1f, 1f, 0.8f));
+    }
+}
---
+++ New Unity Project/Assets/GameManager/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject completeLevelUi;
+
+    private bool isGameEnded = false;
+    private const float restartDelay = 1f;
+
+    public void EndGame()
+    {
+        if(!isGameEnded)
+        {
+            isGameEnded = true;
+            Debug.Log("Game Over!");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    public void CompleteLevel()
+    {
+        Debug.Log("You won!");
+        completeLevelUi.SetActive(true);
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
